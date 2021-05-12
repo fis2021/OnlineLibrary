0a654b39e2f08c5923712e95d185fa5cdf263ea8
UngureanDenisa14 <81463669+UngureanDenisa14@users.noreply.github.com>
2021-05-12 12:59:33 +0300

    view book

---
+++ src/main/java/org/loose/fis/sre/controllers/ViewBookController.java
@@ -0,0 +1,39 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import org.loose.fis.sre.model.ClickedBook;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewBookController implements Initializable {
+    @FXML
+    private ImageView imgView;
+    @FXML
+    private Text titluText;
+    @FXML
+    private TextArea descArea;
+    @FXML
+    private Text lgText;
+    @FXML
+    private Text catText;
+    @FXML
+    private Text genText;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image img=new Image(ClickedBook.selectedBook.getPhoto_path());
+        imgView.setImage(img);
+        titluText.setText(ClickedBook.selectedBook.getTitlu());
+        descArea.setText(ClickedBook.selectedBook.getDescription());
+        descArea.setEditable(false);
+        lgText.setText(ClickedBook.selectedBook.getLimba());
+        catText.setText(ClickedBook.selectedBook.getDom_stiintific());
+        genText.setText(ClickedBook.selectedBook.getGen_literar());
+
+    }
+}
