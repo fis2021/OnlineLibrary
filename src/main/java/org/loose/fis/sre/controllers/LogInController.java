62e9dada0440a01313007d64a67068c1a0b144d2
veroxmocanu <veronica.mocanu@student.upt.ro>
2021-05-15 18:42:36 +0300

    update logincontroller

--- src/main/java/org/loose/fis/sre/controllers/LogInController.java
+++ src/main/java/org/loose/fis/sre/controllers/LogInController.java
@@ -1,32 +1,21 @@
 package org.loose.fis.sre.controllers;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import org.h2.mvstore.cache.CacheLongKeyLIRS;
 import org.loose.fis.sre.exceptions.IncorrectPasswordException;
 import org.loose.fis.sre.exceptions.InvalidUsernameException;
 import org.loose.fis.sre.services.UserService;
 import javafx.scene.control.TextField;
 
-import javax.swing.*;
-
 import java.io.IOException;
-import java.io.ObjectInputFilter;
-//import java.awt.*;
 
-public class LogInController {
+public class LoginController {
     @FXML
     private Label logMessage;
     @FXML
@@ -35,8 +24,6 @@ public class LogInController {
     private PasswordField passwordField;
     @FXML
     private Button logButton;
-    @FXML
-    private Button regButton;
 
     public void handleRegistrationView() throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
@@ -49,48 +36,41 @@ public class LogInController {
         try
         {
             UserService.userExists(usernameField.getText(),passwordField.getText());
-            logMessage.setText("Successful log in");
+            //logMessage.setText("Successful log in");
             if(UserService.checkIsAdmin(usernameField.getText())==true)
             {
                 Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("admin_main_page.fxml"));
                 Stage scene= (Stage) logButton.getScene().getWindow();
                 scene.setTitle("Admin");
-                scene.setScene(new Scene(root,1920,1080));
+                scene.setScene(new Scene(root,1200,1000));
                 scene.setResizable(false);
-                scene.setMinHeight(1080);
-                scene.setMinWidth(1920);
+                scene.setMinHeight(900);
+                scene.setMinWidth(1000);
                 scene.setMaxHeight(1080);
                 scene.setMaxWidth(1920);
                 scene.setTitle("Admin");
                 scene.setFullScreen(true);
-
-
             }
             else
             {
                 Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("client_main_page.fxml"));
                 Stage scene= (Stage) logButton.getScene().getWindow();
-                scene.setScene(new Scene(root,1920,1080));
+                scene.setScene(new Scene(root,1200,1000));
                 scene.setResizable(false);
-                scene.setMinHeight(1080);
-                scene.setMinWidth(1920);
+                scene.setMinHeight(900);
+                scene.setMinWidth(1000);
                 scene.setMaxHeight(1080);
                 scene.setMaxWidth(1920);
                 scene.setTitle("Client");
                 scene.setFullScreen(true);
-
             }
-
-
         }
         catch(InvalidUsernameException e)
         {
             logMessage.setText(e.getMessage());
-
         }catch(IncorrectPasswordException e)
         {
             logMessage.setText(e.getMessage());
         }
-
     }
 }
